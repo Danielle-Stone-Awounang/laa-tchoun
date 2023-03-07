00fad6f1e09dd0d61df5933405a218db8a31c5f7
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-03-07 05:51:46 +0100

    structure

---
+++ recette_africaine/interfaces/chef/acceuil/acceuil.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View,Text,StyleSheet } from 'react-native'
+
+export default function AcceuilChef(){
+    return(
+        <View style={styles.container}>
+            <Text>bonjour les gars</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+})
